b347db9fa558f18f2a0d03e15787bb6c0acf6bf6
DanielArF <daniel_af96@hotmail.com>
2015-09-15 17:00:35 +0200

    add a photo in the lib folder

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
 
 get '/' do
-  'hello!'
+  "<div>
+    <img src='http://bit.ly/1eze8aE'>
+   </div>"
 end
+
+get '/secret' do
+  'Seriously, this is a secret page'
+end
+
+get '/hello' do
+	"Hello, World. Daniel, jodel, how"
+end 
